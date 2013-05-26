ce2c304ee58458a8fb49a1d533501e7f089de5e7
Pablo D. Jadzinsky <jadz@stanford.edu>
2013-05-26 14:11:32 -0700

    Fix code to work with multiple files Seems to work but needs more testing

--- photodiode.py
+++ photodiode.py
@@ -2,28 +2,74 @@ import meaRecording
 import numpy as np
 
 def analysePD(wildcard, whiteThreshold=.8):
+  '''
+    Load and analyse the photodiode associated with all files that match wildcard in current folder
+    
+    Parameters:
+    -----------
+    wildcard:            string to match files, uses fnmatch, see its help if needed but * and ? are acceptable
+    whiteThreshold:      a number between 0 and 1, although the code is not checking for it and any number will make the code work.
+                         whtie frames are defined as any crossing of thresholdm where threshold is whiteTrheshold*maxValueInRecording
+    
+    Output:
+    -------
+    startTimes:      list with times where a stimulus started
+    endTimes:        list with times where a stimulus ended
+    period:          distance in between white frames for a given stimulus
+  '''
   import numpy as np
   
   # generate a ndarray with all the scans corresonding to a white frame and a list with the average number of scans per frame per file
   #
-  whiteFrameScans, scansperframe = loadAllWhiteFrames(wildcard, whiteThreshold)
+  whiteFrameScans, scansperframe, scanRate = loadAllWhiteFrames(wildcard, whiteThreshold)
+  print 'finished loading all whiteFrames from', wildcard
   
   # At this point I have finished loading PD from all files and extracting all possible white frames.
   # Remove those white frames that are of no interest, that where detected because we are probably not flipping stimulus ever frame
   whiteFrameScans = keepOnlyFirstWhite(whiteFrameScans, scansperframe)    # this is a list
   
-  # for readability of following line, wfs is not a new array but a new name for whiteFrameScans
-  wfs = whiteFrameScans
-  
-  # Keep only those white frames that signal the start of a new whiteFrameDistance
-  # next line is somewhat complicated, to understand it right in a piece of paper the sequence of scans corresponding to whiteFrames
+  # Make a list with those items that correspond to the 1st white frame for a given experiment.
+  # By a change in experiment I mean that the period in between white frames changes. It can also change if experiment is skipping frames.
+  # next line is somewhat complicated, to understand it. If needed right in a piece of paper the sequence of scans corresponding to whiteFrames
   # then we want to compute the distance between those consecutive scans but at the end, we care about the change in distance, that
-  # is why we have to compare 3 consecutive whiteFrameScans. And we always want to return the 1st element in the list
-  wfs = [wfs[item] for item in range(len(wfs)-1) if wfs[item+1]+wfs[item-1]-2*wfs[item]>scansperframe/2 or item==0]
+  # is why we have to compare 3 consecutive whiteFrameScans. Also, independently of the computation, we always want to return the 1st element in the list
+  firstWhiteIndex = [item for item in range(len(whiteFrameScans)-1) if whiteFrameScans[item+1]+whiteFrameScans[item-1]-2*whiteFrameScans[item]>scansperframe.mean()/2 or item==0]
   
-  return whiteFrameScans
+  # Make a list with indexes into lastWhiteFrames per stim. This list, as firstWhiteIndex does not hold times not scans but indexes into whiteFrameScans
+  lastWhiteIndex = [firstWhiteIndex[item]-1 for item in range(1,len(firstWhiteIndex))]
+  lastWhiteIndex.append(len(whiteFrameScans)-1)
+  
+  # make the following lists:
+  # Time of 1st white in a given experiment.
+  startTimes = [whiteFrameScans[index]/float(scanRate) for index in firstWhiteIndex]
+  # period between whiteFrames of a given stimulus
+  period = [(whiteFrameScans[index+1] - whiteFrameScans[index])/float(scanRate) for index in firstWhiteIndex]
+  # end of the experiment
+  endTimes = [whiteFrameScans[index]/float(scanRate)+period[i] for i, index in enumerate(lastWhiteIndex)]
+  
+  #                st whites and another with their corresponding periods
+  return startTimes, endTimes, period
 
-def loadAllWhiteFrames(wildcard, whiteThreshold):
+def loadAllWhiteFrames(wildcard, whiteThresholdFactor):
+  '''
+    For all files in current directory that match wildcard, open one at a time and extract all the scans corresponding to PD
+    where whiteThreshold is crossed. For each file, load PD, compute the maximum value in the PD and define as threshold anything that
+    crosses that value * whiteThresholdFactor
+    ***********************
+    VERY IMPORTANT, this will produce random white frames if there are absolutely no white frames in the PD recording
+    ***********************
+    
+    Parameters:
+    -----------
+    whildcard:      a string matching all files to be analyzed, see fnmatch for syntax but (* and ? are accepted)
+    whiteThresholdFactor:  a number between 0 and 1, although the code is not checking for it and any number will work
+    
+    Output:
+    -------
+    whiteFrameScans:     ndarray with the scans corresponding to the white frames
+    scansperframe:       estimated number of scans per frame
+    scanRate:            from the file's header, how many samples per second are we recording.
+    '''
   import os
   import fnmatch
   from numpy import append
@@ -39,6 +85,10 @@ def loadAllWhiteFrames(wildcard, whiteThreshold):
     if fnmatch.fnmatch(file, wildcard):
       print 'Working on file', file
       
+      header = meaRecording.getRecordingHeader(file)
+      nscans = header['nscans']
+      scanRate = header['scanRate']
+      
       # load PD, each item corresponds to 1/scanrate seconds
       pd = loadPD(file)
       
@@ -49,7 +99,7 @@ def loadAllWhiteFrames(wildcard, whiteThreshold):
       pd_max = np.max(pd)
       
       # define threshold that has to be crossed to define a white pd.
-      whiteThreshold = pd_max*whiteThreshold
+      whiteThreshold = pd_max*whiteThresholdFactor
       
       # detect crossings of threshold
       # all crossings are detected, if stimulus is apdating every n frames, each white frame will be detected n times
@@ -59,9 +109,12 @@ def loadAllWhiteFrames(wildcard, whiteThreshold):
       whiteFrameScans += accumulatedScans
       
       # update accumulatedScans
-      accumulatedScans += meaRecording.getRecordingHeader(file)['nscans']
+      accumulatedScans += nscans
+      
+      # append white frames for current file to list with all white frames
+      allWhiteScans = np.append(allWhiteScans, whiteFrameScans)
   
-  return whiteFrameScans, scansperframe
+  return allWhiteScans, scansperframe, scanRate
 
 def findThresholdCrossing(array, threshold):
   '''
@@ -160,19 +213,19 @@ def keepOnlyFirstWhite(whiteFrameScans, scansPerFrame):
     
     Comments:
     ---------
-    whiteFrameScans has the points in the photodiode recording at which threshold was cross.
-    whiteFrameScans is also a link to the time of such events since each point is spaced on average 1/scanRate
-    if framesN[n] != framesN[n-1] it means that a new stimuli started at time whiteFrameTimes[n] and now whiteFrame distances are every framesN[n]
+    whiteFrameScans has the points in the photodiode recording at which threshold was crossed.
+    whiteFrameScans is also a link to the time of such events since each point is spaced on average every 1/scanRate seconds
     '''
   import numpy as np
   # subtract from every element in whiteFrameScans the previous element. Subtract 0 from the 1st element
   dist = whiteFrameScans[:]-np.concatenate([[0], whiteFrameScans[0:-1]])
   
-  whiteFrameScans = [whiteFrameScans[item] for item, distance in enumerate(dist) if distance > scansPerFrame*1.5 or item==0]
+  whiteFrameScans = [whiteFrameScans[item] for item, distance in enumerate(dist) if distance > scansPerFrame.mean()*1.5 or item==0]
   return whiteFrameScans
 
-def whiteFrameDistances(whiteFrameScans, scansperframe):
-  '''
+'''
+  def whiteFrameDistances(whiteFrameScans, scansperframe):
+  ' ''
     compute the distance between two consecutive white frames.
     Take into account that most of the time we are not fliping stimuli every frame and therefore the white frame in the photodiode
     will be repeated. Discard this repetition and just report the distance between the 1st white frames (as if flipping every frame)
@@ -193,7 +246,7 @@ def whiteFrameDistances(whiteFrameScans, scansperframe):
     whiteFrameScans has the points in the photodiode recording at which threshold was cross.
     whiteFrameScans is also a link to the time of such events since each point is spaced on average 1/scanRate
     if framesN[n] != framesN[n-1] it means that a new stimuli started at time whiteFrameTimes[n] and now whiteFrame distances are every framesN[n]
-    '''
+    ' ''
   import numpy as np
   
   framesN=[]
@@ -211,5 +264,4 @@ def whiteFrameDistances(whiteFrameScans, scansperframe):
       accumulated = 0
   
   return framesN, changeScans
-
-
+'''
