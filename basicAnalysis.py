9d4abf4ed7fc4ba62643bbd8928ead8739f2f7d2
Pablo D. Jadzinsky <jadz@stanford.edu>
2014-07-26 19:22:26 -0700

    started coding stim object, init and upsample methods are done

--- basicAnalysis.py
+++ basicAnalysis.py
@@ -1,32 +1,35 @@
 #!/Users/jadz/anaconda/bin/python
-from numpy import concatenate, modf, linspace, ceil, histogram, mod, iterable, alltrue
-import matplotlib.pyplot as plt
-import pdb
+#from numpy import  concatenate as _concatenate, modf as _modf, linspace as _linspace, ceil as _ceil, histogram as _histogram, mod as _mod, iterable as _iterable, random as _random, arange as _arange
+import numpy as _np
+import matplotlib.pyplot as _plt
+import pdb as _pdb
 
 def raster(spikes, period):
     '''
     return a 2D numpy array that has raster X/Y information for the spikes
     '''
-    X, Y = modf(spikes/period)
+    X, Y = _np.modf(spikes/period)
     X = X.reshape(-1,1)
     Y = Y.reshape(-1,1)
 
-    raster = concatenate((X,Y), axis=1)
+    raster = _np.concatenate((X,Y), axis=1)
     return raster
 
 
-def PSTH(spikes, period, repeats, deltaT=.05, plotFlag=0, returnFlag=0):
+def psth(spikes, period, repeats, deltaT=.05, plotFlag=0, returnFlag=0):
     '''
     Compute and return the PSTH. Depending on returnFlag, PSTH and/or x are returned.
         returnFlag: 0, returns both hist and bins
                     1, returns just hist
                     2, returns just bins
     '''
-    #pdb.set_trace()
-    x = linspace(0, period, ceil(period/deltaT))
+    #_pdb.set_trace()
+    x = _np.linspace(0, period, _np.ceil(period/deltaT))
 
-    hist, bins = histogram( mod(spikes, period), bins=x )
+    hist, bins = _np.histogram( _np.mod(spikes, period), bins=x )
+    hist = hist.astype(float)
     hist /= deltaT*repeats
+
 
     if returnFlag == 0:
         return hist, bins
@@ -35,70 +38,130 @@ def PSTH(spikes, period, repeats, deltaT=.05, plotFlag=0, returnFlag=0):
     elif returnFlag == 2:
         return bins
 
-
-def processNested(nestedList, func, *argv, recLevel=0, **kwargv):
+def _sta(stim, spikes, stimDeltaT, staStartT, staDeltaT, staEndT=0):
     '''
-    Spikes, PSTHs, Rasters are probably stored as bunch of nested lists until at the very end, I have an ndarray.
+    Compute the Spike Triggered Average
+    stim:       the stimulus, last dimension is time
+    spikes:     an array of spike times
+    stimDeltaT: time resolution in the stimulus (the difference in between two consecutive stimulus frames)
+    staStartT:     time in seconds to start computing the sta, typically -0.5 (half a second before a spike)
+    staDeltaT:     time resolution for the sta, doesn't have to be the same as in the stimulus. typically .01 (10 ms)
+    staEndT:       time in seconds after a spike to end the sta. Typically 0 but other values are possible. Useful in debugging certain stimuli
+    '''
+    _pdb.set_trace()
+    # how many timePnts do I need for the STA?
+    timePntsOut = _np.ceil((staEndT-staStartT)/staDeltaT)
+
+    # make an index array to link pnts in sta to points in stim
+    pnt2pnt = _np.empty(timePntsOut, int)
+
+    # how many pnts does staStartT correspond to in stim?
+    startPntsIn = -_np.ceil(-staStartT/stimDeltaT)
+
+    # how many pnts does staEndT correspond to in stim?
+    endPntsIn = -_np.ceil(staEndT/staDeltaT)
+
+    # use only spikes that take place in between startPntsIn and endPntsIn
+    spikes = spikes[startPntsIn:-endPntsIn]
+    
+    
+    print(spikes)
+    '''
+    # preallocate the sta
+    sta = _np.empty(stim.shape[-1],timePnts)
+
+    # remove from spikes all spikes for which I will not be able to add the stim staStartT seconds before teh spike or staEndT seconds after the spike
+    spikes = spikes(_np.where(spikes > 
+    # redimension stim to be 2D, collapsing all non time dimensions
+    stim2D = stim.reshape(-1, stim.shape[-1])
+    
+
+    spikeBins = 
+    '''
+
+def processNested(func, nestedArgIndex, *argv, recLevel=0, **kwargv):
+    '''
+    This function runs 'func' with all arguments passed on *argv and **kwargv.
+    The difference between this function and just running func(*argv, **kwargv) is that one of the parameters in *argv is an arbitrarly nested list
+    of lists and what is actually passed to func, is not the nested list but the embeded elements whithin the list (they can't be lists but could
+    be arrays, numbers, sets, strings, etc.). All other parameters in argv will also be passed to func with the cabeat that if any of the parameters
+    is also a list with the same length as the one pointed by nestedArgIndex, it is also parsed when calling func.
+    See the examples and _testProcessNested below for clarification
+
+    Spikes, PSTHs, Rasters are probably stored as bunch of nested lists until at the very end, when spike times are stored in ndarray.
     Traverse through the nested lists until the ndarray and in that case process it with func.
     recLevel is used in recursion and should not be given explicitly when calling this function 
 
-    Usage:
-    L = [arange(3), arange(3,7), arange(7,10)]
-    L2 = processNested(L, lambda x: x**2)
+    Usage: see _testProcessNested below
     '''
     
-    #pdb.set_trace()
-    print(argv)
-    # First of all, work on a deep copy of nestedList.
+    #_pdb.set_trace()
+    #print(argv)
+    # First of all, work on a deep copy of the argument pointed by nestedArgIndex.
     if recLevel == 0:
-        output = nestedList.copy()
-        return processNested(output, func, recLevel=1, *argv, **kwargv)
-
-    assert recLevel>0, "recLevel is not greater than 0, it was" % recLevel
+        output = argv[nestedArgIndex].copy()
+        # replace in argv the element pointed by nestedArgIndex by the copy just created 
+        argv = argv[:nestedArgIndex]+(output,)+argv[nestedArgIndex+1:]
+        return processNested(func, nestedArgIndex, recLevel=1, *argv, **kwargv)
     
-    # if nestedList has method shape, process it with func
-    # I'm assuming that nestedList are nested object (not necessarily lists) but those objects do not have a shape method
-    #pdb.set_trace()
-    try:
-        nestedList.shape
-        #print(id(nestedList))
-        #pdb.set_trace()
-        nestedList = func(nestedList, *argv, **kwargv)
+    # if argv[nestedArgIndex] is not a list, process it with func and all corresponding parameters
+    # I'm assuming that argv[nestedArgIndex] are nested lists of lists until at the very end, the last object is not a list.
+    #_pdb.set_trace()
+    if isinstance(argv[nestedArgIndex], list):
+        #_pdb.set_trace()
+        N = len(argv[nestedArgIndex])
+        # I have to replace element nestedArgIndex from argv, but argv is a tuple and cannot be changed, thereofre construct a new tuple
+        newTupleItem = list(map(lambda i: processNested(func, nestedArgIndex, recLevel=recLevel+1, *_argFromArgs(i, nestedArgIndex, *argv), **kwargv), range(N)))
+        return newTupleItem
+    else:
+        return func(*argv, **kwargv)
 
-        #print(id(nestedList))
-        return nestedList
-    except:
-        #pdb.set_trace()
-        N = len(nestedList)
-        newArgs = argFromArgs(0, argv)
-        print(newArgs)
-        nestedList = list(map(lambda i: processNested(nestedList[i], func, recLevel=recLevel+1, *argFromArgs(i, *argv), **kwargv), range(N)))
-        
-        #nestedList = list(map(lambda x: processNested(x, func, recLevel=recLevel+1, *argv, **kwargv), nestedList))
-        return nestedList
 
-def argFromArgs(i, *argv):
+def _argFromArgs(i, nestedArgIndex, *argv):
     '''
-    argv is a set where each element is either an iterable or not.
-    return another set 'setOut'
-    where setOut[j] is setOut[j][i] if setOut[j] is iterable and setOut[j] if it is not an iterable
+    argv:               a tuple
+    nestedArgIndex:     an index into argv, pointing to the element with the nested list.
+    i:                  an index into argv[nestedArgIndex] (in case argv[nestedArgIndex] is an interable, if i==0 argv[nestedArgIndex] might not be an interable)
     
-    In calling basicAnalysis.procesNested(nestedList, func, argv, ...)
-    I might want to pass the same arg to every element in nestedList or a different value of the same argument to each element in nestedList.
+    output:             another tuple 'tout', where:
+                            if tout[j] is not iterable:
+                                tout[j] is not changed
+                            if len(tout[j]) != len(tout[nestedArgIndex]):
+                                tout[j] is unchanged 
+                            if len(tout[j]) == len(tout[nestedArgIndex]):
+                                tout[j] is replaced by tout[j][i] if tout[j] is iterable and tout[j] if it is not an iterable
+    
+    General idea
+    In calling basicAnalysis.procesNested(func, nestedArgIndex, argv, ...)
+    I might want to pass the same arg to every element in nestedArgIndex or a different value of the same argument to each call of func(*argv, **kwargv)
     I'm envisioning a solution where before calling map
-    I'm going to change argv in such a way that each argument in argv is an iterable with len set to N
-    If any arg in argv is of length N nothing is being done to it.
-    If any arg in argv if not iterable, then it is changed by [arg]*N
-    if any arg is iterable but has more or less items than N an error is raised.
     '''
-    #pdb.set_trace()
-    return tuple([arg[i] if iterable(arg) else arg for arg in argv])
+    #Careful with strings, if an arg is passed to argv and its length matches argv[nestedArgIndex] it will be changed.
+    #Not clear to me what desired behaviour is yet
+    return tuple([arg[i] if _np.iterable(arg) and len(arg)==len(argv[nestedArgIndex]) else arg for arg in argv])
+
+
+def nestArgument(arg, nestedLike):
+    '''
+    This function is meant to be used with processNested.
+    In calling processNested arguments are suppossed to be non iterables or iterables with the same nesting structure as the principal argument nestedArgIndex.
+    If by any chance we want to pass the same iterable to all calls of processNested(nestedArgIndex, someIterable) this will fail
+    THerefore I will generate a new iterable that has the same shape as nestedLike and where each final element will be the iterable to pass to func in processNested.
+    arg:            some argument, most likely an iterable
+    nestedLike:     a nested structure of list, the basic structure will be copied onto output
+    
+    output: nestedArg
+    '''
+
+    ''' No need to write new code for this, just use:
+        processNested(nestedLike, lambda x: arg)
+    '''
 
 def plot2D(list2D, *argv, **kwargv):
     '''
     plot all list2D traces, assumes that each element in list2D is a list with the same number of elements.
     '''
-    #pdb.set_trace()
+    #_pdb.set_trace()
     colsN = len(list2D)
     rowsN = len(list2D[0])
 
@@ -109,8 +172,140 @@ def plot2D(list2D, *argv, **kwargv):
 
     for row in range(rowsN):
         for col in range(colsN):
-            plt.subplot(rowsN, colsN, col + row*colsN + 1)
-            plt.plot(list2D[col][row], *argv, **kwargv)
-            plt.ylim(0, maxV)
+            _plt.subplot(rowsN, colsN, col + row*colsN + 1)
+            _plt.plot(list2D[col][row], *argv, **kwargv)
+            _plt.ylim(0, maxV)
 
+def _testProcessNested(case):
+    arg0 = [['a','b','c'],['d','e'],[['f','g'],['h']]]
+    arg1 = 2
+    arg2 = [[2, 3, 4], [5], [[6], [7]]]
+    arg3 = [[2, 3, 4], 5, [6, [7]]]
+
+    if case == 0:
+        # should return array([2,4,6])
+        return processNested(lambda x,y:x+y, 0, _np.array([1,2,3]), _np.array([1,2,3]))
+    if case == 1:
+        # should return "['hola 1', 'chau 2', '2 3']"
+        return processNested(lambda x, y: "{0} {1}".format(x,y), 0, ['hola', 'chau', '2'], [1,2,3])
+    if case == 2:
+        # should return L
+        return processNested(lambda w, x, y, z: w, 0, arg0, arg1, arg2, arg3)
+    if case == 3:
+        # should return a nested list with the same structure as L but all values equal to p
+        return processNested(lambda w, x, y, z: x, 0, arg0, arg1, arg2, arg3)
+    if case == 4:
+        # should return a nested list with the same structure as L but taking values from arg2, compare with case 5
+        return processNested(lambda w, x, y, z: y, 0, arg0, arg1, arg2, arg3)
+    if case == 5:
+        # should return a nested list with the same structure as L but taking values from arg3, compare with case 4
+        return processNested(lambda w, x, y, z: z, 0, arg0, arg1, arg2, arg3)
+    if case == 6:
+        # should return a list of tuples with the nested structure of arg0 but each tuple uses elements from all args
+        # this is a nice example to see what arguments are passed to func
+        return processNested(lambda w, x, y, z: (w,x,y,z), 0, arg0, arg1, arg2, arg3)
+    if case == 7:
+        # this should faile because we are trying to use as the nestedArgument an integer, see example 8
+        return processNested(lambda w, x, y, z: (w,x,y,z), 1, arg0, arg1, arg2, arg3)
+    if case == 8:
+        return processNested(lambda w, x, y, z: (w,x,y,z), 1, [arg0], [arg1], [arg2], [arg3])
+
+def _test_argFromArgs(case, *argv):
+    if case == 0:
+        # should return (2, 3, 'b')
+        print(_argFromArgs(1,0,[1,2,3],3,['a','b','c']))
+    if case == 1:
+        # should return (1, 'hola', 1)
+        print(_argFromArgs(0, 1, 1, ['hola', 'chau','2'], [1,2,3]))
+    if case == 2:
+        # should return (1, 'chau', 2)
+        print(_argFromArgs(1, 1, 1, ['hola', 'chau','2'], [1,2,3]))
+
+
+def _test0(v, cellsN, case):
+    '''
+    not funcy at all, only works with GaussianNatScene so far
+    many assumptions not documented
+    '''
+    p = v['fixationLength']
+
+    if case==0:
+        # explicitly saying what the period is in each condition
+        out = [[[p*3]*8,[p*4]*8]]*cellsN
+    elif case==1:
+        # explicitly saying what the period is in some conditions but other conditions inherit period from parent condition
+        out = [[p*3, [p*4]*8]]*cellsN
+    elif case==2:
+        # explicitly saying different period in some conditions and inheriting period in other conditions
+        out = [[p, list(abs(_np.random.randn(8)))]]*cellsN
+    elif case==3:
+        out = [[p, list(_np.arange(0.5, 4.1, .5))]]*cellsN
+    
+    return out
+
+def loadSeq(filePath, shape, inputBites):
+    import struct
+    
+    _pdb.set_trace()
+    samplesN = _np.array(shape).prod()
+    with open(filePath, 'rb') as fin:
+        seq = _np.array(struct.unpack('f'*samplesN, fin.read(4*samplesN)))
+
+    seq = seq.reshape(shape, order='F')
+
+
+    return seq
+
+
+def resampleStim(stim, N):
+    '''
+    resample stim in the time domain (assumed to be last dimension in stim) such that last dimension gets expanded to N time samples.
+    
+    each element in output array is equal to an element of stim, the rule is newP = floor(oldP * (oldN-1)/(N-1))
+    
+    stim:   the stimulus you want to resample
+    N:      meaning depends on context
+            if N > stim.shape[-1]
+                then N is the new size of time domain
+            else
+                N is a multiplicative factor, new time domain will be stim.shape[-1]*N
+
+    '''
+    from itertools import product
+
+    # old time samples
+    oldN = stim.shape[-1]
+
+    # newStim time samples
+    if N < oldN:
+        N *= oldN
+
+    # pre allocate new stim
+    newStim = _np.zeros(stim.shape[:-1] + (N,), stim.dtype)
+
+    # generate an index array to connect points in stim to points in newStim. Next lines of code might be a bit tricky. I want to generate N samples in between 0 and oldN-1, but if I do something like linspace(0, oldN-1, N) I get only one sample at oldN-1 and this is not correct. The trick is to generate N+1 samples and make the last sample be too big and discard it
+    index = _np.linspace(0, oldN, N+1)[:-1].astype(int)      # int here plays double duty. On one side I want to floor results from linspace but indexing into array requires arrays of integer or boolean type, not float
+    
+    #_pdb.set_trace()
+    # make an iterable over all coordinates of stim (excluding time)
+    arg = (range(i) for i in stim.shape[:-1])
+    
+    for a in product(*arg):
+        originalSignal = stim[a]
+        newStim[a] = originalSignal[index]
+    
+    return newStim
+
+def getTimeStampArray(d):
+    '''
+    from experimental variables, generate the time stamp array used in pyret
+    d:  a dictionary with the following keys
+        framePeriod
+        waitframes
+        endT
+
+    output: (ndarray) the time stamp array
+    '''
+
+    return _np.arange(0, d['endT'], d['framePeriod']*d['waitframes'])
 
---
+++ stimulustools.py
@@ -0,0 +1,340 @@
+'''
+stimulustools.py
+
+Tools for basic manipulation of stimulus arrays.
+
+(C) 2014 bnaecker, nirum
+'''
+
+import numpy as np
+import pdb as _pdb
+
+class stimulus:
+    def __init__(self, stim, tsa=None, d=None):
+        '''
+        Generate a stimulus object consisting of the ndarray stim and an associated time stamp array (tsa) linking stimulus frames to time.
+
+        stim:   your n dimension stimulus, last dimension is associated with time
+        tsa:    time stamp array coming from the experimental computer
+        d:      dictionary with experimental parameters, relies on having following keys:
+                'endT':             end time of the experiment and assumes the experiment starts at t=0
+                'framePeriod':      average flipping time of the monitor, currently with the monitor set at 10Hz is about 0.0100047
+                'waitframes':       integer describing how over how many monitor flips the stimulus changes.
+        '''
+        self.stim = stim
+        _pdb.set_trace()
+        if tsa is not None:
+            self.tsa = tsa
+            self.deltaT = (tsa[1:]-tsa[:-1]).mean()
+            self.endT = tsa[-1] + self.deltaT
+        elif d:
+            print(d)
+            if 'framePeriod' not in d:
+                raise ValueError('in generating stimulus object passed dictionary does not have "framePeriod" key')
+            if 'waitframes' not in d:
+                raise ValueError('in generating stimulus object passed dictionary does not have "waitframes" key')
+            if 'endT' not in d:
+                raise ValueError('in generating stimulus object passed dictionary does not have "endT" key')
+
+            self.deltaT = d['framePeriod']*d['waitframes']
+            self.endT = d['endT']
+            self.tsa = np.arange(0, self.endT, self.deltaT)
+        else:
+            raise ValueError('either tsa or endT and deltaT are needed parameters')
+    
+    def upsamplestim(self, upfact):
+        '''
+
+        Upsample the given stimulus by the given factor.
+
+        Input
+        -----
+        upfact (int):
+            The upsample factor.
+        
+        output
+        ------
+        stim object after upsampling time dimension by 'upfact'
+
+        '''
+
+        # Compute old and new sizes
+        oldsz   = self.stim.shape
+        newsz   = oldsz[:-1] + (upfact * oldsz[-1],)
+
+        # Upsample the stimulus array
+        stim_us = (self.stim.reshape((-1, 1)) * np.ones((1, upfact))).reshape(newsz)
+
+        # Upsample the time stamp array
+        x       = np.arange(0, upfact * self.tsa.size)
+        xp      = np.arange(0, upfact * self.tsa.size, upfact)
+        time_us = np.interp(x, xp, np.squeeze(self.tsa))
+
+        stimObj_us = stimulus(stim_us, tsa=time_us)
+
+        return stimObj_us
+
+def downsamplestim(stim, downfact, time=None):
+    '''
+
+    Downsample the given stimulus by the given factor.
+
+    Input
+    -----
+
+    stim (ndarray):
+        The original stimulus array
+
+    downfact (int):
+        The factor by which the stimulus will be downsampled
+
+    time (ndarray) [optional]:
+        The time axis of the original stimulus
+
+    Output
+    ------
+
+    stim_ds (ndarray), time_ds (ndarray):
+        The downsampled time vector and stimulus array
+
+    '''
+
+    # Downsample the stimulus array
+    stim_ds = np.take(stim, np.arange(0, stim.shape[-1], downfact), axis=-1)
+    
+    # Downsample the time vector, if given
+    time_ds = time[::downfact] if time is not None else None
+
+    return stim_ds, time_ds
+
+def upsamplestim(stim, upfact, time=None):
+    '''
+
+    Upsample the given stimulus by the given factor.
+
+    Input
+    -----
+
+    stim (ndarray):
+        The actual stimulus to be upsampled.
+
+    upfact (int):
+        The upsample factor.
+
+    time (ndarray) [optional]:
+        The time axis of the original stimulus.
+
+    Output
+
+    stim_us (ndarray), time_us (ndarray):
+        The upsampled time vector and stimulus array
+
+    '''
+
+    # Compute old and new sizes
+    oldsz   = stim.shape
+    newsz   = oldsz[:-1] + (upfact * oldsz[-1],)
+
+    # Upsample the stimulus array
+    stim_us = (stim.reshape((-1, 1)) * np.ones((1, upfact))).reshape(newsz)
+
+    # Upsample the time vecctor if given
+    if time is not None:
+        x       = np.arange(0, upfact * time.size)
+        xp      = np.arange(0, upfact * time.size, upfact)
+        time_us = np.interp(x, xp, np.squeeze(time))
+
+    else:
+        time_us = None
+
+    return stim_us, time_us
+
+def downsamplestim(stim, downfact, time=None):
+    '''
+
+    Downsample the given stimulus by the given factor.
+
+    Input
+    -----
+
+    stim (ndarray):
+        The original stimulus array
+
+    downfact (int):
+        The factor by which the stimulus will be downsampled
+
+    time (ndarray) [optional]:
+        The time axis of the original stimulus
+
+    Output
+    ------
+
+    stim_ds (ndarray), time_ds (ndarray):
+        The downsampled time vector and stimulus array
+
+    '''
+
+    # Downsample the stimulus array
+    stim_ds = np.take(stim, np.arange(0, stim.shape[-1], downfact), axis=-1)
+    
+    # Downsample the time vector, if given
+    time_ds = time[::downfact] if time is not None else None
+
+    return stim_ds, time_ds
+
+def slicestim(stim, history, locations=None, tproj=None):
+    '''
+
+    Slices a spatiotemporal stimulus array (over time) into overlapping frames.
+
+    Input
+    -----
+
+    stim (ndarray):
+        The spatiotemporal or temporal stimulus to slices. Should have shape
+        (n, n, t) or (t,).
+
+    history (int):
+        Integer number of time points to keep in each slice.
+
+    locations (boolean) [optional]:
+        Boolean array of temporal locations at which slices are taken. If unspecified,
+        use all time points.
+
+    tproj (ndarray) [optional]:
+        Matrix of temporal filters to project stimuli onto
+
+    Output
+    ------
+
+    slices (ndarray):
+        Array of stimulus slices, with all stimulus dimensions collapsed into one. 
+        That is, it has shape (np.prod(stim.shape), `history`)
+
+    '''
+
+    # Collapse any spatial dimensions of the stimulus array
+    cstim = stim.reshape(-1, stim.shape[-1])
+
+    # Check history is an int
+    if history != int(history):
+        raise ValueError('"history" must be an integer')
+    history = int(history)
+
+    # Compute spatial locations to take
+    if locations is None:
+        locations = np.ones(cstim.shape[-1])
+    
+    # Don't include first `history` frames regardless
+    locations[:history] = False
+
+    # Construct full stimulus slice array
+    if tproj is None:
+
+        # Preallocate
+        slices = np.empty((int(history * cstim.shape[0]), int(np.sum(locations[history:]))))
+
+        # Loop over requested time points
+        for idx in np.where(locations)[0]:
+            slices[:, idx-history] = cstim[:, idx - history :idx].ravel()
+
+    # Construct projected stimulus slice array
+    else:
+
+        # Preallocate
+        slices = np.empty((int(tproj.shape[1] * cstim.shape[0]), int(np.sum(locations[history:]))))
+
+        # Loop over requested time points
+        for idx in np.where(locations)[0]:
+
+            # Project onto temporal basis
+            slices[:, idx-history] = (cstim[:, idx-history:idx].dot(tproj)).ravel()
+
+    return slices
+
+def getcov(stim, history, tproj=None, cutoff=0.1):
+    '''
+
+    Computes a stimulus covariance matrix
+    ** Warning: this is computationally expensive for large stimuli **
+
+    Input
+    -----
+
+    stim (ndarray):
+        The spatiotemporal or temporal stimulus to slices. Should have shape
+        (n, n, t) or (t,).
+
+    history (int):
+        Integer number of time points to keep in each slice.
+    
+    cutoff (default=0.1):
+        The cutoff for small singular values in computing the inverse covariance matrix
+
+    tproj (ndarray):
+        Temporal basis set to use. Must have # of rows (first dimension) equal to history.
+        Each extracted stimulus slice is projected onto this basis set, which reduces the size
+        of the corresponding covariance matrix to store.
+
+    Output
+    ------
+
+    cov (ndarray):
+        (n*n*t by n*n*t) Covariance matrix
+
+    covinv (ndarray):
+        (n*n*t by n*n*t) Inverse covariance matrix (computed using the pseudoinverse)
+
+    '''
+
+    # temporal basis (if not given, use the identity matrix)
+    if tproj is None:
+        tproj = np.eye(history)
+
+    if tproj.shape[0] != history:
+        raise ValueError('The first dimension of the basis set tproj must equal history')
+
+    # Collapse any spatial dimensions of the stimulus array
+    cstim = stim.reshape(-1, stim.shape[-1])
+
+    # store mean + covariance matrix
+    mean = np.zeros(cstim.shape[0] * tproj.shape[1])
+    cov = np.zeros((cstim.shape[0] * tproj.shape[1], cstim.shape[0]*tproj.shape[1]))
+
+    # pick some indices to go through
+    indices = np.arange(history,cstim.shape[1])
+    numpts  = np.min(( cstim.shape[0]*tproj.shape[1]*10, indices.size ))
+    np.random.shuffle(indices)
+
+    # loop over temporal indices
+    for j in range(numpts):
+
+        # pick which index to use
+        idx = indices[j]
+        print('[%i of %i]' % (j,numpts))
+
+        # get this stimulus slice, projected onto the basis set tproj
+        stimslice = cstim[:, idx - history : idx].dot(tproj).reshape(-1,1)
+
+        # update the mean
+        mean += np.squeeze(stimslice)
+
+        # add it to the covariance matrix
+        cov += stimslice.dot(stimslice.T)
+
+    # normalize and compute the mean outer product
+    mean = mean / (cstim.shape[1] - history)
+    mean_op = mean.reshape(-1,1).dot(mean.reshape(1,-1))
+
+    # mean-subtract and normalize the STC by the number of points
+    cov = (cov / (cstim.shape[1]-history)) - mean_op
+
+    # compute the inverse covariance
+    try:
+        covinv = np.linalg.pinv(cov, cutoff)
+    except np.linalg.LinAlgError:
+        print('Warning: could not compute the inverse covariance.')
+        covinv = None
+
+    return cov, covinv
+
