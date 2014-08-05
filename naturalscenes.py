01518bea44f3eff1ae55845653d1a8d810e3f7d2
Pablo D. Jadzinsky <jadz@stanford.edu>
2014-08-05 16:09:27 -0700

    Modified code to process many images and many cells per image It pre-allocates a 2D array for the linear prediction of all expected cells (not fully tested) and populates the 2D array one cell at a time I also included a function to generate a sample of randomly picked g values

--- naturalscenes.py
+++ naturalscenes.py
@@ -6,6 +6,7 @@ A module to load and process natural scenes from Tkacik database
 import numpy as _np
 from scipy import ndimage as _nd
 from glob import glob as _glob
+from itertools import product as _product
 import matplotlib.pyplot as _plt
 import pdb as _pdb
 
@@ -17,10 +18,10 @@ clipK = 117             # point at which kernels are clipped
 deltaT = .005           # time resolution of kernels in seconds
 rwStep = .01            # in degrees
 saccadeSize = 6         # in degrees
-centerS = 1             # in degrees
+centerS = 1             # diameter, in degrees
 surroundS = 2.5         # scale factor, surround is surroundS bigger than centerS
 surroundW = 1           # weight of surround pathway relative to center one (center pathway has weight=1)
-startT = -.2            # in seconds
+startT = -2             # in seconds
 endT = 2                # in seconds
 
 def getImagesPath(path=None):
@@ -111,19 +112,48 @@ def getEyeSeq():
 
     return seq.astype('int16')
 
-def getOneLP(imNumber, centerX, centerY):
-    '''
-    simulate the linear prediction (g) for one cell centered on (centerX, centerY) in imNumber moving according to getEyeSeq()
+def processAllImages():
+    if imList == '':
+        getImagesPath()
+
+    # estimate number of cells per image
+    surroundD = surroundS*centerS*pixperdegree
+    imSize = loadImage(0).shape
+    cellsN = _np.floor(imSize[0]/surroundD)*_np.floor(imSize[1]/surroundD)
+
+    # compute time axis of g
+    tax = _np.arange(startT, endT, deltaT)
+
+    # preallocate array for all linear predictions
+    g = _np.empty((cellsN*len(imList), len(tax)))
+   
+    #_pdb.set_trace()
+    nextCell = 0
+    for imNumber in range(len(imList)):
+        print(imList[imNumber])
+        nextCell = processOneImage(imNumber, g, nextCell)
+        
+        if imNumber==10:
+            break
     
+    g = g[:nextCell][:]
+    return g
+
+def processOneImage(imNumber, g, nextCell):
+    '''
+    Compute the linear prediction of several cells moving over the image described by imNumber
+
     inputs:
     -------
         imNumber:   integer, image to load from imList
+    
+        g:          2D array with all inear predictions. Will be modified in place
 
-        centerX/Y:  integer, pixel describing the cell's center postion
-
+        nextCell:    index into the 1st dimension of g where next simulated cell should be incorporated.
+                    
     output:
-    -------
-        g:          ndarray, the linear prediction 
+        g:          modified in place, incorporates the linear predictions from image imNumber in g, starting from 
+                    row = nextCell
     '''
 
     # filter image with center and surround spatial filters. filterImage takes inputs in degrees, not pixels
@@ -133,9 +163,55 @@ def getOneLP(imNumber, centerX, centerY):
     # grab the eye movement sequence
     seq = getEyeSeq()
 
+    # grab non overlapping cells from image such that when moved according to seq, they are always whithing the boundaries
+    centerD = centerS/2*pixperdegree    # center's diameter in pixels
+    surroundD = surroundS*centerD        # surround's diameter in pixels
+
+    startX = _np.ceil(surroundD - min(seq[0][:]))
+    endX = _np.floor(imC.shape[0] - surroundD - max(seq[0][:]))
+    startY = _np.ceil(surroundD - min(seq[1][:]))
+    endY = _np.floor(imC.shape[1] - surroundD - max(seq[1][:]))
+    
+    i = 0
+    for center in _product(_np.arange(startX, endX, centerD), _np.arange(startY, endY, centerD)):
+        #print('processing image centered on: {0}'.format(center))
+        #g[nextCell, :] = getOneLP(imC, imS, seq, center)#, g, nextCell)
+        getOneLP(imC, imS, seq, center, g, nextCell)
+        nextCell += 1
+        i+=1
+        if i==2:
+            return nextCell
+
+    return nextCell
+
+def getOneLP(centerIm, surroundIm, seq, center, g, nextCell):
+    '''
+    simulate the linear prediction (g) for one cell centered on center = (centerX, centerY) in imNumber moving according to getEyeSeq()
+    
+    inputs:
+    -------
+        centerIm:   2D ndarray, image to be processed after filtering with center spatial filter
+
+        surroundIm: 2D ndarray, image to be processed after filtering with surround spatial filter
+
+        seq:        2D ndarray, sequence of positions relative to centerX, centerY
+                    seq[0][:] are positions in x
+                    seq[1][:] are positions in y
+
+        center:     tuple with 2 integers, (x, y) describing the cell's center postion in the image
+
+        g:          2D ndarray, holds all linear predictions and is modified in place
+
+        nextCell:   index into g where next linear prediction will be inserted
+        
+    output:
+    -------
+        g:          ndarray, the linear prediction 
+    '''
+
     # for the given cell center, extract the sequence of luminance values as seen by the center and the surround
-    centerG = _np.array([imC[seq[0,i]][seq[1,i]] for i in range(seq.shape[1])])
-    surroundG = _np.array([imS[seq[0,i]][seq[1,i]] for i in range(seq.shape[1])])
+    centerG = _np.array([centerIm[seq[0,i]+center[0]][seq[1,i]+center[1]] for i in range(seq.shape[1])])
+    surroundG = _np.array([surroundIm[seq[0,i]+center[0]][seq[1,i]+center[1]] for i in range(seq.shape[1])])
 
     # Filter the center and the surround by its corresponding kernel
     if len(centerK)==0 or len(surroundK)==0:
@@ -144,14 +220,15 @@ def getOneLP(imNumber, centerX, centerY):
     centerG = _np.convolve(centerG, centerK, mode='valid')
     surroundG = _np.convolve(surroundG, surroundK, mode='valid')
     
-    g = centerG + surroundW*surroundG
-
-    plot(g, centerG, surroundG)
-
-def plot(*argv):
-    tax = _np.array(_np.arange(startT, endT, deltaT))
+    #_pdb.set_trace()
+    g[nextCell,:] = centerG + surroundW*surroundG
     
-    _plt.close('all')
-    _plt.figure('g')
-    for arg in argv:
-        _plt.plot(tax, arg)
+
+def sampleG(g, num):
+    x = _np.arange(startT, endT, deltaT)
+    print(x.shape, g.shape)
+    _plt.close('Sample G')
+    _plt.figure('Sample G')
+    for i in range(num):
+        index = _np.random.randint(0, g.shape[0])
+        _plt.plot(x, g[index,:])
